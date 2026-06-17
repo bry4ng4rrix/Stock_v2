f0aef0d4d00f8614c3bec805c0b2ebfb2692bc7d
Bryan_garrix_dev <66459466+bry4ng4rrix@users.noreply.github.com>
2026-06-17 14:54:51 +0300

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-background to-muted p-4">
       
-        <div>cody nitesteko anle docker</div>
+      
         <LoginForm />
     </div>
   );
